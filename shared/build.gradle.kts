c01546ac03a24247d9384ecf68a2f62f41b7648f
DonizeteVida <donizetevida1998@outlook.com>
2026-05-15 01:49:10 +0200

    Add navigation3 and adaptative suite dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -57,8 +57,10 @@ kotlin {
         implementation(libs.compose.uiToolingPreview)
         implementation(libs.androidx.lifecycle.viewmodelCompose)
         implementation(libs.androidx.lifecycle.runtimeCompose)
-
         implementation(libs.kotlinx.datetime)
+
+        implementation(libs.navigation3.core)
+        implementation(libs.navigation3.adaptive.navigation.suite)
 
         testImplementation(libs.kotlin.test)
     }
