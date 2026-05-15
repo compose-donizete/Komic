20043559f8a4ff2a2203903d65984d0b47780f97
DonizeteVida <donizetevida1998@outlook.com>
2026-05-15 18:20:50 +0200

    Add dark/light theme support

---
+++ shared/src/commonMain/kotlin/Navigation.kt
@@ -0,0 +1,69 @@
+package com.dv.apps.komic
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
+import androidx.compose.material3.adaptive.navigationsuite.rememberNavigationSuiteScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.ui.NavDisplay
+import komic.shared.generated.resources.*
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.StringResource
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+
+@Composable
+fun Navigation() {
+    val state = rememberNavigationSuiteScaffoldState()
+    val stack = mutableStateListOf(Destination.HOME)
+
+    NavigationSuiteScaffold(
+        state = state,
+        navigationSuiteItems = {
+            Destination.entries.forEach { destination ->
+                item(
+                    selected = stack.single() == destination,
+                    onClick = { stack.fill(destination) },
+                    icon = {
+                        Icon(
+                            painterResource(
+                                if (stack.single() == destination) {
+                                    destination.selectedIcon
+                                } else {
+                                    destination.icon
+                                }
+                            ),
+                            contentDescription = stringResource(destination.title)
+                        )
+                    },
+                    label = { Text(destination.name) }
+                )
+            }
+        }
+    ) {
+        NavDisplay(
+            stack,
+            entryProvider = entryProvider {
+                entry<Destination> {
+                    when (it) {
+                        Destination.HOME -> Text("HOMEa")
+                        Destination.SHELF -> Text("SHELF")
+                        Destination.SETTINGS -> Text("SETTINGS")
+                    }
+                }
+            }
+        )
+    }
+}
+
+enum class Destination(
+    val title: StringResource,
+    val icon: DrawableResource,
+    val selectedIcon: DrawableResource
+) {
+    HOME(Res.string.menu_home, Res.drawable.ic_home, Res.drawable.ic_home_filled),
+    SHELF(Res.string.menu_shelf, Res.drawable.ic_shelf, Res.drawable.ic_shelf_filled),
+    SETTINGS(Res.string.menu_settings, Res.drawable.ic_settings, Res.drawable.ic_settings_filled)
+}
