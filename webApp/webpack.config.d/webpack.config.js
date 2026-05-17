774735a902cb74ac862fd248875247709b0d1f17
DonizeteVida <donizetevida1998@outlook.com>
2026-05-17 15:01:49 +0200

    Check webpack.config.js against releases' builds

--- webApp/webpack.config.d/webpack.config.js
+++ webApp/webpack.config.d/webpack.config.js
@@ -1,6 +1,6 @@
-;(function(config) {
+if (config.devServer) {
     config.devServer.headers = [
         { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
         { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' }
     ]
-})(config);
+}
